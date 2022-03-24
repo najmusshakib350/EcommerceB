641aef232d3c48e9d4ee4778adde242a308fd716
shakib <shakibn350@gmail.com>
2022-03-24 19:56:26 +0600

    Ecommerce version 01

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,3 +1,11 @@
 module.exports = (err, req, res, next) => {
-    return res.status(500).send("Something Failed!!!!");
-}
+  console.log("Hello i am global error");
+  console.log(err);
+  console.log("Hello global error message");
+  console.log(err.message);
+  //return res.status(500).send("Something Failed!!!!");
+  return res.status(500).json({
+    err,
+    errorMessage: err.message,
+  });
+};
